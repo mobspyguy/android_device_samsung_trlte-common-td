e1ca30a7a2cc7625dd41b0ea1eb7c15848a3d197
Nicholas Magill <nmagill1979@gmail.com>
2019-07-29 23:28:18 +0100

    Specify kernel compiler prefix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,7 @@ BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_KERNEL_CONFIG := apq8084_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/apq8084
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_RAMDISK_OFFSET     := 0x02600000
 BOARD_KERNEL_TAGS_OFFSET := 0x02400000
